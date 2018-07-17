76d20be3ea165f18c5ed83584855dccba287528d
evagradovich <33654687+evagradovich@users.noreply.github.com>
2018-07-17 12:42:56 +0100

    Update fastq_quality_assessment.sh

--- fastq_quality_assessment.sh
+++ fastq_quality_assessment.sh
@@ -1,3 +1,11 @@
+#initially have all FASTQ files compressed in GZIP format (fastq.gz) in one folder. To unzip:
+
+for f in *.gz; do gunzip $f; done
+
+#now have FASTQ files in .fastq format. 
+#Run FastQC program on all files (generates a HTML report which can be visually assessed manually)
+
+
 for fq in *.fastq
 do
 ~/FastQC/fastqc $fq
