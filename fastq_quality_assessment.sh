538d37cd15da0573677eb6f207a80059906db08e
evagradovich <33654687+evagradovich@users.noreply.github.com>
2018-07-17 12:51:07 +0100

    Update fastq_quality_assessment.sh

--- fastq_quality_assessment.sh
+++ fastq_quality_assessment.sh
@@ -1,3 +1,23 @@
+# FastQC program can be downloaded here:
+#  https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.7.zip
+
+# To install the program, unzip the folder into desired directory, 
+#    unzip fastqc_v0.11.7.zip -d destination_folder
+#    cd destination_folder/fastqc_v0.11.7
+#    chmod 755 fastqc
+#
+#  when using FastQC give it the path to the fastqc script which is now executable
+
+
+
+
+
+
+
+
+
+
+
 #initially have all FASTQ files compressed in GZIP format (fastq.gz) in one folder. To unzip:
 
 for f in *.gz; do gunzip $f; done
