49c6236df0e1e8158fa00ad50c99f6bb0061bf92
evagradovich <33654687+evagradovich@users.noreply.github.com>
2018-07-17 11:26:59 +0100

    Update read_quantification.sh

--- read_quantification.sh
+++ read_quantification.sh
@@ -2,6 +2,8 @@
 
 # subread installed locally
 
+module load R
+
 
 /project/home17/eg1914/Downloads/subread-1.6.2-source/bin/featureCounts" "-T" "4" "-p" "-t" "exon" "-g" "gene_id" "-a" "/project/home17/eg1914/my_rnaseq/gencode.v28.annotation.gtf" "-o" "all_counts_featureCounts.txt" *.bam #all BAM files in same directory
 
@@ -9,3 +11,6 @@
 
 
 cut -f 1,7-30 all_counts_featureCounts.txt > featureCounts_all_counts_clean.txt
+
+
+Rscript deseq.R
