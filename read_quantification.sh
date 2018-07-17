8cbbc26432c53ba588b8ebc2b5dc72a980b5202b
evagradovich <33654687+evagradovich@users.noreply.github.com>
2018-07-17 11:26:07 +0100

    Create read_quantification.sh

---
+++ read_quantification.sh
@@ -0,0 +1,11 @@
+# done using featureCounts (Subread software or Rsubread package)
+
+# subread installed locally
+
+
+/project/home17/eg1914/Downloads/subread-1.6.2-source/bin/featureCounts" "-T" "4" "-p" "-t" "exon" "-g" "gene_id" "-a" "/project/home17/eg1914/my_rnaseq/gencode.v28.annotation.gtf" "-o" "all_counts_featureCounts.txt" *.bam #all BAM files in same directory
+
+# trim the resultant file, only keep the read counts
+
+
+cut -f 1,7-30 all_counts_featureCounts.txt > featureCounts_all_counts_clean.txt
