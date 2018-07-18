22ed72ae42144ee8296f881119e418ebea239888
evagradovich <33654687+evagradovich@users.noreply.github.com>
2018-07-18 11:12:55 +0100

    Update infusion.sh

--- infusion.sh
+++ infusion.sh
@@ -2,3 +2,5 @@
 
 
 bowtie2-build  --threads 35 /data/eg1914/genome/GENCODE/GRCh38.p12.genome.fa  bowtie2_genome_infusion
+
+./setup_reference_dataset.py -gtf ~/Downloads/Homo_sapiens.GRCh38.90.gtf -f /data/eg1914/genome/GENCODE/GRCh38.p12.genome.fa  -t ~/Downloads/Homo_sapiens.GRCh38.cdna.all.fa -fIdx /data/eg1914 -tIdx ~/Downloads -o /data/eg1914/infusion/ --num-threads 35 --adj-cdna-ids
