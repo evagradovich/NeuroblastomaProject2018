e9c4f857a14ae18d066d7f9c6906836e056acac6
evagradovich <33654687+evagradovich@users.noreply.github.com>
2018-07-23 14:32:01 +0100

    Create eriscript.sh

---
+++ fusion_transcripts/eriscript.sh
@@ -0,0 +1,1 @@
+./ericscript.pl -db ~/Documents/NB_results/ericscript/database/ericscript_db -name S1 -o ~/Documents/NB_results/ericscript/results/ ~/my_rnaseq/FASTQ/1_S1_R1_001.fastq ~/my_rnaseq/FASTQ/1_S1_R2_001.fastq
